4ed03ac717fa9b2371d6a99a749ad38412b55ab5
Amirmasoud <amirmasood33@gmail.com>
2022-03-09 18:20:41 +0330

    Update User model

--- cloud/Domain/IAM/Models/User.php
+++ cloud/Domain/IAM/Models/User.php
@@ -3,9 +3,13 @@
 namespace Domain\IAM\Models;
 
 use Domain\IAM\QueryBuilders\UserQueryBuilder;
+use Domain\Sites\Models\Server;
 use Domain\Sites\Models\Site;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,41 +18,27 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'provider',
-        'provider_id',
-        'provider_token',
-        'provider_refresh_token',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    public function sites(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function servers(): HasMany
+    {
+        return $this->hasMany(Server::class);
+    }
+
+    public function sites(): HasMany
     {
         return $this->hasMany(Site::class);
     }
