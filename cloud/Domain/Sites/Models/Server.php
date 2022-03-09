fa54309232d442db7983439d9e49d0e3bc5c1a85
Amirmasoud <amirmasood33@gmail.com>
2022-03-09 18:16:03 +0330

    Fix server model

--- cloud/Domain/Sites/Models/Server.php
+++ cloud/Domain/Sites/Models/Server.php
@@ -3,35 +3,51 @@
 namespace Domain\Sites\Models;
 
 use Domain\IAM\Models\User;
-use Domain\Sites\QueryBuilders\SiteQueryBuilder;
+use Domain\Sites\Enums\DiskTypeEnum;
+use Domain\Sites\Enums\DiskUnitEnum;
+use Domain\Sites\Enums\OsFlavorEnum;
+use Domain\Sites\Enums\RamUnitEnum;
+use Domain\Sites\Enums\ServerStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
-use Support\Containers\Enums\ContainerState;
+use Spatie\Tags\HasTags;
 
 class Server extends Model
 {
     use HasFactory;
     use SoftDeletes;
     use LogsActivity;
+    use HasTags;
 
     protected $fillable = [
         'name',
-        'host',
+        'price',
+        'ip',
+        'port',
+        'ram',
+        'ram_unit',
+        'cpu',
+        'disk',
+        'disk_type',
+        'disk_unit',
+        'os',
+        'os_version',
         'status',
+        'notes',
     ];
 
     protected $casts = [
-        'status' => ContainerState::class,
-    ];
-
-    protected $observables = [
-        'started',
-        'stopped',
-        'restarted',
+        'price' => 'integer',
+        'port' => 'integer',
+        'ram_unit' => RamUnitEnum::class,
+        'disk_type' => DiskTypeEnum::class,
+        'disk_unit' => DiskUnitEnum::class,
+        'os' => OsFlavorEnum::class,
+        'status' => ServerStatusEnum::class,
     ];
 
     public function user(): BelongsTo
