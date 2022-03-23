582eed2a13c6ac9f3f152209a62342da988b15ad
Amirmasoud <amirmasood33@gmail.com>
2022-03-23 20:55:15 +0430

    Improve server request

--- cloud/App/Dashboard/Requests/ServerRequest.php
+++ cloud/App/Dashboard/Requests/ServerRequest.php
@@ -2,6 +2,10 @@
 
 namespace App\Dashboard\Requests;
 
+use Domain\Sites\Enums\DiskTypeEnum;
+use Domain\Sites\Enums\DiskUnitEnum;
+use Domain\Sites\Enums\OsFlavorEnum;
+use Domain\Sites\Enums\RamUnitEnum;
 use Domain\Sites\Enums\ServerStatusEnum;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Enum;
@@ -21,12 +25,12 @@ class ServerRequest extends FormRequest
             'ip' => ['required'],
             'port' => ['required'],
             'ram' => ['required'],
-            'ram_unit' => ['required'],
+            'ram_unit' => ['required', new Enum(RamUnitEnum::class)],
             'cpu' => ['required'],
             'disk' => ['required'],
-            'disk_type' => ['required'],
-            'disk_unit' => ['required'],
-            'os' => ['required'],
+            'disk_type' => ['required', new Enum(DiskTypeEnum::class)],
+            'disk_unit' => ['required', new Enum(DiskUnitEnum::class)],
+            'os' => ['required', new Enum(OsFlavorEnum::class)],
             'os_version' => ['required'],
             'status' => ['required', new Enum(ServerStatusEnum::class)],
             'notes' => ['required'],
