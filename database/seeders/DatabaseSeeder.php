ec58c587fc271d9e9494977f7ea64340ebc12279
Amirmasoud <amirmasood33@gmail.com>
2022-06-26 18:31:56 -0600

    Add basic roles and permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@cloud.test',
-            'password' => bcrypt('secret'),
-        ]);
+        if (! User::whereEmail('admin@cloud.test')->exists()) {
+            User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'admin@cloud.test',
+                'password' => bcrypt('secret'),
+            ]);
+        }
 
         User::factory(100)->create();
+
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+
+        // create roles and assign created permissions
+
+        Role::create(['name' => 'writer'])
+            ->givePermissionTo('edit articles');
+
+        Role::create(['name' => 'moderator'])
+            ->givePermissionTo(['publish articles', 'unpublish articles']);
+
+        Role::create(['name' => 'super-admin'])
+            ->givePermissionTo(Permission::all());
+    }
+}
