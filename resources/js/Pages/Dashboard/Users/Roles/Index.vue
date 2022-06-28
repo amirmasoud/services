e94e340090a4fe7c1a8cba611eaa3b16b8cc6529
Amirmasoud <amirmasood33@gmail.com>
2022-06-27 19:16:38 -0600

    Fix table endpoint

--- resources/js/Pages/Dashboard/Users/Roles/Index.vue
+++ resources/js/Pages/Dashboard/Users/Roles/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppHead title="Users" />
+  <AppHead title="Users"/>
 
   <DashboardMain>
     <template #header>Users</template>
@@ -8,25 +8,25 @@
     </template>
     <template #sub-header></template>
     <template #sub-header-content>
-      <Stats :stats="stats" />
+      <Stats :stats="stats"/>
     </template>
 
     <Table
-      :records="records"
-      :filters="filters"
-      endpoint="/dashboard/users"
-      :fields="table.fields"
       :actions="table.actions"
+      :endpoint="$route('dashboard.users.roles.index')"
+      :fields="table.fields"
+      :filters="filters"
+      :records="records"
     />
   </DashboardMain>
 </template>
 
 <script setup>
-import Table from "@/Components/Table";
 import AppHead from "@/Components/AppHead";
+import Button from "@/Components/Button";
 import DashboardMain from "@/Components/DashboardMain";
 import Stats from "@/Components/stats";
-import Button from "@/Components/Button";
+import Table from "@/Components/Table";
 
-defineProps({ records: Object, filters: Object, table: Object, stats: Object });
+defineProps({records: Object, filters: Object, table: Object, stats: Object});
 </script>
