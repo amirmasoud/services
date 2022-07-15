1716bbdaef4e681b5e822bde0d33ebb5aab97e6b
Amirmasoud <amirmasood33@gmail.com>
2022-07-14 23:48:35 -0600

    Improve seeder by handling exceptions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Domain\Users\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\QueryException;
+use Spatie\Permission\Exceptions\PermissionAlreadyExists;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if (! User::whereEmail('admin@cloud.test')->exists()) {
+        try {
             User::factory()->create([
                 'name' => 'Admin',
                 'email' => 'admin@cloud.test',
                 'password' => bcrypt('secret'),
             ]);
+        } catch (QueryException) {
+            $this->command->warn('Admin user already exists, skipping');
         }
 
-        User::factory(100)->create();
+        try {
+            User::factory(100)->create();
+        } catch (QueryException) {
+            $this->command->warn('Fake user already exists, skipping');
+        }
 
-        $this->call([
-            RolesAndPermissionsSeeder::class,
-        ]);
+        try {
+            $this->call([
+                RolesAndPermissionsSeeder::class,
+            ]);
+        } catch (PermissionAlreadyExists) {
+            $this->command->warn("Permission already exists, skipping");
+        }
     }
 }
--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -20,20 +20,14 @@ class RolesAndPermissionsSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
+        Permission::findOrCreate('edit articles');
+        Permission::findOrCreate('delete articles');
+        Permission::findOrCreate('publish articles');
+        Permission::findOrCreate('unpublish articles');
 
         // create roles and assign created permissions
-
-        Role::create(['name' => 'writer'])
-            ->givePermissionTo('edit articles');
-
-        Role::create(['name' => 'moderator'])
-            ->givePermissionTo(['publish articles', 'unpublish articles']);
-
-        Role::create(['name' => 'super-admin'])
-            ->givePermissionTo(Permission::all());
+        Role::findOrCreate('writer')->givePermissionTo('edit articles');
+        Role::findOrCreate('moderator')->givePermissionTo(['publish articles', 'unpublish articles']);
+        Role::findOrCreate('super-admin')->givePermissionTo(Permission::all());
     }
 }
