09f70561667e522da1f81e08bfa12064040d8778
Amirmasoud <amirmasood33@gmail.com>
2023-02-27 23:32:44 -0700

    Basic functionality

--- cloud/App/Dashboard/Controllers/DashboardController.php
+++ cloud/App/Dashboard/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Dashboard\Controllers;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -10,6 +11,9 @@ class DashboardController extends Controller
 {
     public function __invoke(): Response
     {
-        return Inertia::render('Dashboard/TheHome');
+        return Inertia::render('Dashboard/TheHome', [
+            'diff_1' => Carbon::parse('2025-01-10')->diffAsCarbonInterval(now()),
+            'diff_2' => Carbon::parse('2024-05-31')->diffAsCarbonInterval(now()),
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/TheHome.vue
+++ resources/js/Pages/Dashboard/TheHome.vue
@@ -6,6 +6,8 @@ import route from "ziggy-js";
 
 defineProps({
   stats: Object,
+  diff_1: Object,
+  diff_2: Object,
 });
 </script>
 <template>
@@ -16,6 +18,12 @@ defineProps({
     <template #sub-header-content>
       <Stats :stats="stats"/>
     </template>
+    {{ diff_1?.y }} years {{ diff_1?.m }} month {{ diff_1?.d }} days {{ diff_1?.h }} hours {{ diff_1?.i }} minutes
+    {{ diff_1?.s }} seconds
+    <br/>
+    {{ diff_2?.y }} years {{ diff_2?.m }} month {{ diff_2?.d }} days {{ diff_2?.h }} hours {{ diff_2?.i }} minutes
+    {{ diff_2?.s }} seconds
+    <br/>
     <Link :href="route('logout')" as="button" method="post">Logout</Link>
   </DashboardMain>
 </template>
