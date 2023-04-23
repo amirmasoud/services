24978565031ac068de884040552cf79e34787d7d
Amirmasoud <amirmasood33@gmail.com>
2023-04-23 02:59:40 -0600

    clock

---
+++ resources/js/Components/Core/MenuBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheClock from "@/Components/Core/Clock/TheClock.vue"</script>
+<template>
+  <section class="core-menu-bar">
+    <div></div>
+    <TheClock class="text-sm flex items-center"/>
+    <div></div>
+  </section>
+</template>
+<style scoped>
+.core-menu-bar {
+  @apply bg-transparent h-7 flex m-1 rounded px-3;
+  @apply flex justify-between items-center;
+}
+</style>
--- resources/js/Pages/Dashboard/TheHome.vue
+++ resources/js/Pages/Dashboard/TheHome.vue
@@ -3,6 +3,7 @@ import AppHead from "@/Components/AppHead.vue";
 import DashboardMain from "@/Components/DashboardMain.vue";
 import Stats from "@/Components/stats.vue";
 import route from "ziggy-js";
+import MenuBar from "@/Components/Core/MenuBar.vue";
 
 defineProps({
   stats: Object,
@@ -13,17 +14,14 @@ defineProps({
 <template>
   <AppHead title="Home"/>
 
+  <MenuBar/>
+
   <DashboardMain>
     <template #header>Home</template>
     <template #sub-header-content>
       <Stats :stats="stats"/>
     </template>
-    {{ diff_1?.y }} years {{ diff_1?.m }} month {{ diff_1?.d }} days {{ diff_1?.h }} hours {{ diff_1?.i }} minutes
-    {{ diff_1?.s }} seconds
-    <br/>
-    {{ diff_2?.y }} years {{ diff_2?.m }} month {{ diff_2?.d }} days {{ diff_2?.h }} hours {{ diff_2?.i }} minutes
-    {{ diff_2?.s }} seconds
-    <br/>
+
     <Link :href="route('logout')" as="button" method="post">Logout</Link>
   </DashboardMain>
 </template>
